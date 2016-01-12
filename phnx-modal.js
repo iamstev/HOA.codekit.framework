65ca3de0e19530783e179f5ad72f4f800ae5f4c2
Stev <topshelfsmith@gmail.com>
2016-01-12 10:10:46 -0500

    added pieces necessary for Connect site
    still needs cleanup and organization

---
+++ phnx-modal.js
@@ -0,0 +1,34 @@
+// VERSION 1.0.5
+
+
+function showModal(div, blackoutcolor) {
+    "use strict";
+    if (typeof blackoutcolor !== 'undefined') {
+		document.getElementById('blackout').style.backgroundColor = blackoutcolor;
+    }
+    document.getElementById('blackout').style.display = 'block';
+    document.getElementById(div).style.display = 'block';
+    $('body').addClass('stop-scroll');
+    document.openModal = div;
+}
+function hideModal(div) {
+    "use strict";
+    document.getElementById('blackout').style.display = 'none';
+    document.getElementById('blackout').style.backgroundColor = 'black';
+	document.getElementById(div).style.display = 'none';
+	document.getElementById('modal_content').innerHTML = '';
+    document.getElementById('modal_h1').innerHTML = '';
+    $('body').removeClass('stop-scroll');
+    document.openModal = '';
+}
+
+
+function fullScreenLoad(action) {
+    "use strict";
+    if (action === 'show') {
+        document.getElementById('fullscreenload').style.display = 'block';
+    }
+    if (action === 'hide'){
+        document.getElementById('fullscreenload').style.display = 'none';
+    }
+}
